95898677718c29f490d315ff4653b9bbe6184991
Steve Jorgensen <stevej@stevej.name>
2014-06-29 17:43:02 -0700

    Add utf8 encoding directive to .rb files

--- lib/client_for_poslynx.rb
+++ lib/client_for_poslynx.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 require "client_for_poslynx/version"
 
 module ClientForPoslynx
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
+# coding: utf-8
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'client_for_poslynx'
