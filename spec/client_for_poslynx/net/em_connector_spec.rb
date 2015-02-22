a036a6c4763c4d45df5eb2facf52f842694701cd
Steve Jorgensen <stevej@stevej.name>
2015-02-22 11:22:24 -0800

    Reorder examples in spec for EM_Connector

--- spec/client_for_poslynx/net/em_connector_spec.rb
+++ spec/client_for_poslynx/net/em_connector_spec.rb
@@ -43,68 +43,6 @@ module ClientForPoslynx
 
       describe '#connect' do
 
-        context "with SSL" do
-          let( :use_ssl ) { true }
-
-          it "initiates a secure connection" do
-            expect( em_system ).to receive( :connect ) { |*args|
-              expect( args.length ).to eq( 4 )
-              host, port, handler, listener = args
-
-              expect( host ).to eq( :the_host )
-              expect( port ).to eq( :the_port )
-              expect( handler ).to eq( subject.connection_class )
-              expect( listener ).to respond_to( :to_em_connector_callback_adapter )
-              @connection_handler = handler.new( listener )
-            }
-
-            subject.connect
-
-            expect( @connection_handler ).to receive( :start_tls ).with( verify_peer: false )
-
-            @connection_handler.connection_completed
-          end
-
-          context "with response" do
-            include_context "Simulates EM::connect()"
-
-            before do
-              subject.connect callback
-
-              expect( callback ).to receive( :call ) do |handler, success|
-                expect( handler ).to eq( @connection_handler )
-                @success = success
-              end
-            end
-
-            context "on successful http connection" do
-              before do
-                allow( @connection_handler ).to receive( :start_tls )
-                @connection_handler.connection_completed
-              end
-
-              it "calls back with connection handler and success on successful ssl start" do
-                @connection_handler.ssl_handshake_completed
-                expect( @success ).to eq( true )
-              end
-
-              it "calls back with connection handler and failure on ssl failure" do
-                @connection_handler.unbind
-                expect( @success ).to eq( false )
-              end
-            end
-
-            context "on http connection failure" do
-              before do ; @connection_handler.unbind ; end
-
-              it "calls back with connection handler and failure" do
-                expect( @success ).to eq( false )
-              end
-            end
-          end
-
-        end
-
         context "without SSL" do
           include_context "Simulates EM::connect()"
 
@@ -201,6 +139,68 @@ module ClientForPoslynx
               end
             end
           end
+        end
+
+        context "with SSL" do
+          let( :use_ssl ) { true }
+
+          it "initiates a secure connection" do
+            expect( em_system ).to receive( :connect ) { |*args|
+              expect( args.length ).to eq( 4 )
+              host, port, handler, listener = args
+
+              expect( host ).to eq( :the_host )
+              expect( port ).to eq( :the_port )
+              expect( handler ).to eq( subject.connection_class )
+              expect( listener ).to respond_to( :to_em_connector_callback_adapter )
+              @connection_handler = handler.new( listener )
+            }
+
+            subject.connect
+
+            expect( @connection_handler ).to receive( :start_tls ).with( verify_peer: false )
+
+            @connection_handler.connection_completed
+          end
+
+          context "with response" do
+            include_context "Simulates EM::connect()"
+
+            before do
+              subject.connect callback
+
+              expect( callback ).to receive( :call ) do |handler, success|
+                expect( handler ).to eq( @connection_handler )
+                @success = success
+              end
+            end
+
+            context "on successful http connection" do
+              before do
+                allow( @connection_handler ).to receive( :start_tls )
+                @connection_handler.connection_completed
+              end
+
+              it "calls back with connection handler and success on successful ssl start" do
+                @connection_handler.ssl_handshake_completed
+                expect( @success ).to eq( true )
+              end
+
+              it "calls back with connection handler and failure on ssl failure" do
+                @connection_handler.unbind
+                expect( @success ).to eq( false )
+              end
+            end
+
+            context "on http connection failure" do
+              before do ; @connection_handler.unbind ; end
+
+              it "calls back with connection handler and failure" do
+                expect( @success ).to eq( false )
+              end
+            end
+          end
+
         end
       end
 
