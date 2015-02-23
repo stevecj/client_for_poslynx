a0196bf0b318728861d7bb57a08538101ecd355f
Steve Jorgensen <stevej@stevej.name>
2015-02-22 18:37:16 -0800

    Create EM_Connector class

---
+++ lib/client_for_poslynx/net/em_connector.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+require_relative 'structured_client/em_connection'
+
+module ClientForPoslynx
+  module Net
+
+    class EM_Connector
+
+    end
+
+  end
+end
---
+++ spec/client_for_poslynx/net/em_connector_spec.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+module ClientForPoslynx
+
+  describe Net::EM_Connector do
+
+
+  end
+
+end
