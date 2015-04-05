ec212fe49ac03da4b351a6a901ee0946c6c5f56c
Steve Jorgensen <stevej@stevej.name>
2015-04-04 22:17:19 -0700

    Improve the basic example

--- examples/talk_to_fake_pos_term.rb
+++ examples/talk_to_fake_pos_term.rb
@@ -29,32 +29,55 @@ connector = EM_Connector.new(SERVER_IP, SERVER_PORT, encryption: ENCRYPTION)
 EventMachine.run do
 
   EM_Session.execute connector do |s|
-    request_data = Requests::PinPadDisplayMessage.new.tap{ |r|
-      r.client_mac = CLIENT_MAC
-      r.line_count = 2
-      r.text_lines = ["How done", "do you want it"]
-      r.button_labels = %w(Rare Medium Well-done)
-    }
-    response = s.request( request_data )
+    begin
+      request_data = Requests::PinPadDisplayMessage.new.tap{ |r|
+        r.client_mac = CLIENT_MAC
+        r.line_count = 2
+        r.text_lines = ["How done", "do you want it"]
+        r.button_labels = %w(Rare Medium Well-done)
+      }
+      response = s.request( request_data )
+      unless response.error_code == '0000'
+        puts "Got failure response from 1st request"
+        puts "Error code: #{response.error_code}. Result text: #(response.result}"
+        return
+      end
 
-    request_data = Requests::PinPadDisplayMessage.new.tap{ |r|
-      r.client_mac = CLIENT_MAC
-      r.line_count = 2
-      r.text_lines = [
-        "Got result: #{response.result}",
-        "Got button response: #{response.button_response}"
-      ]
-    }
-    s.request request_data
+      request_data = Requests::PinPadDisplayMessage.new.tap{ |r|
+        r.client_mac = CLIENT_MAC
+        r.line_count = 2
+        r.text_lines = [
+          "Got result: #{response.result}",
+          "Got button response: #{response.button_response}"
+        ]
+      }
+      s.request request_data
+      unless response.error_code == '0000'
+        puts "Got failure response from 2nd request"
+        puts "Error code: #{response.error_code}. Result text: #(response.result}"
+        return
+      end
 
-    s.sleep 5
+      s.sleep 4
 
-    request_data = Requests::PinPadReset.new.tap{ |r|
-      r.client_mac = CLIENT_MAC
-    }
-    s.request request_data
+      request_data = Requests::PinPadReset.new.tap{ |r|
+        r.client_mac = CLIENT_MAC
+      }
+      s.request request_data
+      unless response.error_code == '0000'
+        puts "Got failure response from concluding reset request"
+        puts "Error code: #{response.error_code}. Result text: #(response.result}"
+        return
+      end
 
-    EventMachine.stop_event_loop
+    rescue EM_Session::RequestError => e
+      puts "An exception was encountered in the session"
+      puts e.message
+
+    ensure
+      EventMachine.stop_event_loop
+
+    end
   end
 
 end
